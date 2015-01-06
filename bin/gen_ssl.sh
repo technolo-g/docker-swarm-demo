8602ac5d77379270c081c9b2d108182b8b4cda6f
Matt Bajor <mbajor@rallydev.com>
2015-01-06 15:29:20 -0700

    Update pull images

--- bin/gen_ssl.sh
+++ bin/gen_ssl.sh
@@ -11,12 +11,6 @@ openssl req -new -passin pass:password \
         -subj '/CN=Non-Prod Test CA/C=US' \
         -x509 -days 365 -key ca-key.pem -out ca.pem
 
-echo 'Creating Docker client certificates (client-key.pem, client-cert.pem)'
-openssl genrsa -des3 -passout pass:password -out client-key.pem 2048
-openssl req -passin pass:password -subj '/CN=client' -new -key client-key.pem -out client.csr
-echo extendedKeyUsage = clientAuth > extfile.cnf
-openssl x509 -passin pass:password -req -days 365 -in client.csr -CA ca.pem -CAkey ca-key.pem -out client-cert.pem -extfile extfile.cnf
-openssl rsa -passin pass:password -in client-key.pem -out client-key.pem
 
 echo 'Creating Swarm certificates (swarm-key.pem, swarm-cert.pem)'
 openssl genrsa -des3 -passout pass:password -out swarm-key.pem 2048
@@ -24,6 +18,10 @@ openssl req -passin pass:password -subj '/CN=dockerswarm01' -new -key swarm-key.
 echo 'extendedKeyUsage = clientAuth,serverAuth' > extfile.cnf
 openssl x509 -passin pass:password -req -days 365 -in swarm-client.csr -CA ca.pem -CAkey ca-key.pem -out swarm-cert.pem -extfile extfile.cnf
 openssl rsa -passin pass:password -in swarm-key.pem -out swarm-key.pem
+
+# Set the default keys to be Swarm
+cp -rp swarm-key.pem key.pem
+cp -rp swarm-cert.pem cert.pem
 
 echo 'Creating host certificates (dockerhost01-5-key.pem, dockerhost01-5-cert.pem)'
 openssl genrsa -passout pass:password -des3 -out dockerhost01-key.pem 2048
@@ -62,4 +60,3 @@ rm -rf ../ansible/roles/docker_swarm/files/tls
 mkdir -p ../ansible/roles/docker_swarm/files/tls
 mv swarm* ../ansible/roles/docker_swarm/files/tls/
 cp -rp ca.pem ../ansible/roles/docker_swarm/files/tls/
-
