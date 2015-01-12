babe3ba6fae4ff56aacac72a82343bc0b4502dbb
Matt Bajor <mbajor@rallydev.com>
2015-01-11 21:18:10 -0700

    Updating the bin dir

---
+++ bin/pull_images_tls.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export DOCKER_CERT_PATH="`pwd`/tls"
+export DOCKER_TLS_VERIFY=1
+
+docker -H tcp://dockerhost01:2376 pull ubuntu:latest &
+docker -H tcp://dockerhost02:2376 pull ubuntu:latest &
+docker -H tcp://dockerhost03:2376 pull ubuntu:latest &
+docker -H tcp://dockerhost04:2376 pull ubuntu:latest &
+docker -H tcp://dockerhost05:2376 pull ubuntu:latest
+
+docker -H tcp://dockerhost01:2376 pull nginx:latest &
+docker -H tcp://dockerhost02:2376 pull nginx:latest &
+docker -H tcp://dockerhost03:2376 pull nginx:latest &
+docker -H tcp://dockerhost04:2376 pull nginx:latest &
+docker -H tcp://dockerhost05:2376 pull nginx:latest
+
